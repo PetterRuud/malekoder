1f269165fa268a56a82b6209b8ed5373cf638ad9
Petter Ruud <petter.ruud@nav.no>
2019-10-04 16:02:02 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <List />
+  <div id="app" :style="cssVars">
+    <List v-on:event_child="eventChild" />
   </div>
 </template>
 
@@ -11,27 +11,53 @@ export default {
   name: "app",
   components: {
     List
+  },
+  methods: {
+		eventChild: function(item) {
+      console.log(item);
+      this.bg = item.hex;
+		},
+  },
+  
+  data() {
+    return {
+      bg: null,
+    }
+  },
+  computed: {
+    cssVars() {
+      return {
+         '--bg': this.bg,
+      };
+    }
   }
 };
 </script>
 
 <style lang="postcss">
+
+:root {
+  --bg: #1a1a1a;
+}
+
+* {
+	box-sizing: border-box;
+}
+
 body {
   margin: 0;
   padding: 0;
+  text-align: left;
+  line-height: 1.4;
+  font-size: 16px;
 }
 
 #app {
   min-height: 100vh;
-  background: #1a1a1a;
+  background: var(--bg);
+  transition: background 500ms ease-in-out;
   font-family: monospace;
   font-weight: bold;
   padding: 2em;
-  font-size: 14px;
-}
-h1 {
-  margin-top: 0;
-  margin-bottom: 1em;
-  color: white;
 }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -4,14 +4,22 @@
       class="color"
       v-bind:key="item.fargekode"
       v-for="item in items"
-      :style="{ backgroundColor: item.hex }"
+      v-on:mouseover="onClickColor(item)"
     >
-      <div class="info">
-        <div>{{ item.fargenavn }}</div>
-        <div>{{ item.produktnavn }}</div>
-        <div>{{ item.jotun }} {{ item.ncs }}</div>
-        <div>{{ item.rom }}</div>
-        <div>{{ item.kjøpt }}</div>
+      <div class="inner">
+        <div class="front" :style="{ backgroundColor: item.hex }">
+          <div class="details">
+            <h3 class="title">{{ item.fargenavn }}</h3>
+          </div>
+        </div>
+
+        <div class="back">
+          <div>{{ item.fargenavn }}</div>
+          <div>{{ item.produktnavn }}</div>
+          <div>{{ item.jotun }} {{ item.ncs }}</div>
+          <div>{{ item.rom }}</div>
+          <div>{{ item.kjøpt }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -26,6 +34,11 @@ export default {
     return {
       items: paints
     };
+  },
+  methods: {
+     onClickColor: function(item) {
+      this.$emit('event_child', item)
+    }
   }
 };
 </script>
@@ -34,7 +47,7 @@ export default {
 .container {
   display: block;
   display: grid;
-  grid-gap: 1em;
+  grid-gap: 2em;
   place-items: center;
   grid-template-columns: repeat(auto-fill, minmax(50%, 1fr));
 
@@ -50,17 +63,61 @@ export default {
     grid-template-columns: repeat(auto-fill, minmax(15%, 1fr));
   }
 
-  .color {
+  .color, 
+  .front, 
+  .back {
+    height: 250px;
+    width: 250px;
+    border-radius: 4px;
     display: block;
-    width: 100%;
-    background-color: white;
-    box-shadow: 0 0 0 2px white;
-    border-radius: 2px;
-    padding-top: 250px;
+    
+  }
 
-    .info {
-      padding: 12px;
-      background-color: white;
+  .color {
+    perspective: 500;
+    &:hover .inner {
+		  transform: rotateY(180deg);
+	  }
+
+    .inner {
+      transition: 0.6s;
+      transform-style: preserve-3d;
+      position: relative;
+
+      .front, 
+      .back {
+        backface-visibility: hidden;
+        box-shadow: 0 2px 0 rgba(0, 0, 0, .06);
+        left: 0;
+        position: absolute;
+        top: 0;
+        border: 8px solid white;
+      }
+
+      .front {
+	      z-index: 2;
+
+        .details {
+          background-color: white;
+          bottom: 0;
+          padding: 16px 12px 12px;
+          position: absolute;
+          width: 100%;
+
+          .title {
+            color: black;
+            font-size: 1.1rem;
+            margin: 0;
+            position: relative;
+            text-transform: uppercase;
+          }
+        }
+      }
+      .back {
+        transform: rotateY(180deg);
+        background-color: white;
+        padding: 12px;
+      }
     }
   }
 }
